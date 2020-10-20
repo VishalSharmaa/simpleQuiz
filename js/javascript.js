90cede640eb28ab90265a4c4595ac10224b6df47
vishal <vksharmaa999@gmail.com>
2020-10-20 19:47:44 +0530

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,52 @@
+var randomNumber = Math.floor(Math.random()*10)+1;
+
+if(randomNumber === 1){
+  document.write("Everyday i want to fly stay by my side. Everday i want to dream stay by side. Every morning i wish i could just play. Wish the morning would just stay.");
+
+}
+else if(randomNumber === 2){
+  document.write("Don't waste the time, Because time is precious");
+  document.getElementById('deco').style.color =('red');
+
+
+}
+else if(randomNumber === 3){
+  document.write("Keep your eyes on the stars and your feets on the ground");
+  document.getElementById('deco').style.color =('#CF2B4C');
+
+}
+else if(randomNumber === 4){
+  document.write("Quality is not a act, it's a habit");
+  document.getElementById('deco').style.color =('#53534F');
+
+}
+
+else if(randomNumber === 5){
+  document.write("Well done is better than well said");
+  document.getElementById('deco').style.color =('#B85E0E');
+
+}
+else if(randomNumber === 6){
+  document.write("Never give up");
+  document.getElementById('deco').style.color =('#7E7A22');
+
+}
+else if(randomNumber === 7){
+  document.write("What you do today can improve all your tomorrows");
+  document.getElementById('deco').style.color =('#237D92');
+
+}
+else if(randomNumber === 8){
+  document.write("Either you run the day or day runs you ");
+  document.getElementById('deco').style.color =('#1D4B21');
+
+}
+else if(randomNumber === 9){
+  document.write("Be kind whenever possible. It is always possible");
+  document.getElementById('deco').style.color =('#922372');
+
+}
+else if(randomNumber === 10){
+  document.write("Start where you are. Use What you have. Do what you can");
+  document.getElementById('deco').style.color =('#237D92');
+}
